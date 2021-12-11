23e62eb70b720bc4165d8057200d83ee7ed2c905
gautam <nathgoutam932gmail.com>
2021-12-12 01:39:35 +0530

    'made it work somehow, lol'

--- src/script.js
+++ src/script.js
@@ -1,105 +1,198 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import './style.css';
+var THREE = require('three');
+var initializeDomEvents = require('threex-domevents/threex.domevents');
+var THREEx = initializeDomEvents(THREE);
 
-// Debug
-const gui = new dat.GUI()
+const { GLTFLoader } = require('three/examples/jsm/loaders/GLTFLoader.js');
+const {
+  OrbitControls,
+} = require('three/examples/jsm/controls/OrbitControls.js');
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+//canvas
+const canvas = document.querySelector('canvas.webgl');
 
-// Scene
-const scene = new THREE.Scene()
-
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
-
-// Materials
-
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
-
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
-
-// Lights
-
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
-
-/**
- * Sizes
- */
+//canvasSize
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: canvas.parentElement.clientWidth,
+  height: canvas.parentElement.clientHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+//clock
+const clock = new THREE.Clock();
+let mixer = null;
+let actions = [];
+let currentAction = 1;
+let model = null;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+let target = new THREE.Vector3();
+let mouseX = 0,
+  mouseY = 0;
+//scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xaaccff);
+scene.fog = new THREE.FogExp2(0xaaccff, 0.0007);
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+//camera
+const camera = new THREE.PerspectiveCamera(
+  55,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+);
+camera.position.set(0, 0, 2);
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
+//renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+var domEvents = new THREEx.DomEvents(camera, renderer.domElement);
+
+//GLTF loader
+const modelLoader = new GLTFLoader();
+
+// model
+modelLoader.load(
+  'inland.glb',
+  (gltf) => {
+    model = gltf.scene;
+    model.castShadow = true;
+    model.receiveShadow = true;
+    model.position.set(0, 0, 0);
+    scene.add(model);
+
+    gltf.scene.traverse(function (child) {
+      if (child.isMesh) {
+        domEvents.addEventListener(child, 'mouseover', () => {
+          spin();
+        });
+      }
+    });
+
+    mixer = new THREE.AnimationMixer(model);
+    let clips = gltf.animations;
+
+    for (let i = 0; i < clips.length; i++) {
+      let clip = clips[i];
+      let action = mixer.clipAction(clip);
+      if (i !== 1) {
+        action.setLoop(THREE.LoopOnce);
+      }
+      actions.push(action);
+    }
+
+    actions[1].play();
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+//lights
+const light = new THREE.AmbientLight(0x404040);
+scene.add(light);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enableZoom = false;
+controls.minPolarAngle = Math.PI / 2;
+controls.maxPolarAngle = Math.PI / 2;
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+const btn_sayHi = document.getElementById('wave');
+btn_sayHi.addEventListener('click', sayHi);
 
-/**
- * Animate
- */
+const btn_spin = document.getElementById('spin');
+btn_spin.addEventListener('click', spin);
 
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+function sayHi() {
+  if (currentAction !== 3) {
+    actions[currentAction].fadeOut(0.2);
+    currentAction = 3;
+    actions[currentAction]
+      .reset()
+      .setEffectiveTimeScale(1)
+      .setEffectiveWeight(1)
+      .fadeIn(0.2)
+      .play();
+    mixer.addEventListener('finished', restoreState);
+  }
 }
 
-tick()
+function spin() {
+  if (currentAction !== 2 && currentAction !== 0) {
+    actions[currentAction].fadeOut(0.2);
+
+    Math.random() > 0.5 ? (currentAction = 2) : (currentAction = 0);
+
+    actions[currentAction]
+      .reset()
+      .setEffectiveTimeScale(1)
+      .setEffectiveWeight(1)
+      .fadeIn(0.2)
+      .play();
+    mixer.addEventListener('finished', restoreState);
+  }
+}
+
+function restoreState() {
+  mixer.removeEventListener('finished', restoreState);
+
+  actions[currentAction].fadeOut(0.2);
+  currentAction = 1;
+
+  actions[currentAction]
+    .reset()
+    .setEffectiveTimeScale(1)
+    .setEffectiveWeight(1)
+    .fadeIn(0.2)
+    .play();
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+function onDocumentMouseMove(event) {
+  mouseX = event.clientX - window.innerWidth / 2;
+  mouseY = event.clientY - window.innerHeight / 2;
+}
+window.addEventListener('mousemove', onDocumentMouseMove);
+
+const tick = () => {
+  // Render
+  renderer.render(scene, camera);
+
+  const delta = clock.getDelta();
+
+  // controls.update();
+
+  mixer?.update(delta);
+
+  target.x = mouseX;
+  target.y = -mouseY;
+  target.z = camera.position.z + 1000; // assuming the camera is located at ( 0, 0, z );
+
+  model?.lookAt(target);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
